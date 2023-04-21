8c4fdf0cdaeb212eaf1f63167b017fd1c0e1e2a4
Alvaro <ag587635@gmail.com>
2023-04-21 13:44:10 -0600

    pool json 1st version

--- SearchEngine/concept/Car Tagger Model/descriptions.py
+++ SearchEngine/concept/Car Tagger Model/descriptions.py
@@ -4,7 +4,19 @@
 import cars
 from cars import cars_dict
 
-words_pull = ["sedan", "coupe", "camioneta"]
+
+class WordPooll:
+    def __init__(self, words):
+        self.words_pull = words
+
+    def get_pool(self, name):
+        return self.words_pull
+
+    def add_to_pool(self, words):
+        self.words_pull.extend(words)
+
+    def replace_pool(self, new_pool):
+        words_pull = new_pool
 
 
 def get_pull():
